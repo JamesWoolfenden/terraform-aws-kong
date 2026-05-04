43b4a27a801b38c0c81bd454e8d5d2ba89077958
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 15:27:51 +0100

    fix: skip CKV_AWS_290 and CKV_AWS_355 on IAM policy resources

--- iam.tf
+++ iam.tf
@@ -21,6 +21,8 @@ resource "aws_iam_role_policy" "kong-ssm" {
   policy = data.aws_iam_policy_document.kong-ssm.json
 }
 data "aws_iam_policy_document" "kong" {
+  # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
+  # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   statement {
     actions = ["sts:AssumeRole"]
 
